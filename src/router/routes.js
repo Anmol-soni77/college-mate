7ba9767e0fa2b10d2d460b250dbe3bb7bd7cc832
anmol <Anmol1111soni@gmail.com>
2023-04-05 12:53:04 +0530

    justupdating

--- src/router/routes.js
+++ src/router/routes.js
@@ -171,7 +171,7 @@ router.post("/signup",async (req,res)=>{
 //     }
 // })
 
-router.get("/login",(req,res)=>{
+router.get("/login",(req,res)=>{ 
     res.render('login',{
         content:""
     }) 
