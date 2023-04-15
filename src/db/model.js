ad749619a0bc62683c9afa7c7802c6619d0996fe
anmol <Anmol1111soni@gmail.com>
2023-04-15 20:01:41 +0530

    establish connections in routes 2

--- src/db/model.js
+++ src/db/model.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const validator = require('validator')
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
+// EXECUTING THE CONNECTION FILES
+require("./db/connection");
+
 
 // DECLEARING THE PACKAGES
 const clientSchema = new mongoose.Schema({
